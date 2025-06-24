d43ac895d86e8ddff6ab5c5372d48e9b1a314a5a
uesen42 <umutesen123@gmail.com>
2025-06-24 18:52:55 +0300

    Update Tests.tsx

--- src/pages/Tests.tsx
+++ src/pages/Tests.tsx
@@ -1,5 +1,5 @@
 // src/pages/Tests.tsx
-
+import { exportTestToPDF } from '@/utils/pdfExport';
 import React, { useState, useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
